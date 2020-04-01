84e3848b7b762ee54a527741171f6ea72799e67b
mykytam <morar.nikita@gmail.com>
2020-04-01 21:48:06 +0300

    Remaking the basics of the project

--- src/main/java/com/mykytam/learningspring/business/service/ReservationService.java
+++ src/main/java/com/mykytam/learningspring/business/service/ReservationService.java
@@ -10,14 +10,17 @@ import com.mykytam.learningspring.data.repository.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Service
 public class ReservationService {
-
     private RoomRepository roomRepository;
     private GuestRepository guestRepository;
     private ReservationRepository reservationRepository;
+    private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
 
     @Autowired
     public ReservationService(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
@@ -26,8 +29,8 @@ public class ReservationService {
         this.reservationRepository = reservationRepository;
     }
 
-    public List<RoomReservation> getRoomReservationsForDate(Date date) {
-        // populate data model
+    public List<RoomReservation> getRoomReservationsForDate(String dateString){
+        Date date = this.createDateFromDateString(dateString);
         Iterable<Room> rooms = this.roomRepository.findAll();
         Map<Long, RoomReservation> roomReservationMap = new HashMap<>();
         rooms.forEach(room->{
@@ -55,5 +58,19 @@ public class ReservationService {
             roomReservations.add(roomReservationMap.get(roomId));
         }
         return roomReservations;
+    }
+
+    private Date createDateFromDateString(String dateString){
+        Date date = null;
+        if(null!=dateString) {
+            try {
+                date = DATE_FORMAT.parse(dateString);
+            }catch(ParseException pe){
+                date = new Date();
+            }
+        }else{
+            date = new Date();
+        }
+        return date;
     }
 }
--- src/main/java/com/mykytam/learningspring/data/entity/Guest.java
+++ src/main/java/com/mykytam/learningspring/data/entity/Guest.java
@@ -28,7 +28,9 @@ public class Guest {
         return id;
     }
 
-    public void setId(long id) { this.id = id; }
+    public void setId(long id) {
+        this.id = id;
+    }
 
     public String getFirstName() {
         return firstName;
